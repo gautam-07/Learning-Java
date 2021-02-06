ce5c8ae7c37b7c83340898be230db293be3be565
Gautam <gomzekh@gmail.com>
2021-02-06 15:01:42 +0530

    First JAVA program

---
+++ basics1.java
@@ -0,0 +1,12 @@
+// Sum of three numbers. First JAVA program
+
+public class basics1 {
+     
+   public static void main(String[] args) {
+      System.out.print("Sum of these numbers is: ");
+      int num1 = 6;
+      int num2 = 7;
+      int num3 = 8;
+      System.out.println(num1 + num2 + num3);
+   }
+}
